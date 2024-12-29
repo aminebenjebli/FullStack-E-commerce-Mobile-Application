db516c6e36e14b67817b7057a66fead66f73695f
aminebenjebli <80284883+aminebenjebli@users.noreply.github.com>
2024-12-29 22:48:55 +0100

    Setup the project

---
+++ ecommerceapp/lib/src/routing/app_route.dart
@@ -0,0 +1,29 @@
+import 'package:ecommerceapp/src/screens/custom_profile_screen.dart';
+import 'package:ecommerceapp/src/screens/custom_sign_in_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+enum AppRoute {
+  signIn,
+  home,
+  profile,
+}
+final goRouterProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: '/sign-in',
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+        path: '/sign-in',
+        name: AppRoute.signIn.name,
+        builder: (context, state) 
+          => const CustomSignInScreen()
+          ),
+      GoRoute(
+         path: 'profile',
+            name: AppRoute.profile.name,
+            builder: (context, state) => const CustomProfileScreen(),
+          ),
+    ],
+  );
+});
