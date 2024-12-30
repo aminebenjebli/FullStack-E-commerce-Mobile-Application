89df8cd7edb19c3c5c216db9faa4b714a6c6be45
aminebenjebli <80284883+aminebenjebli@users.noreply.github.com>
2024-12-30 15:55:23 +0100

    fix Sdk gradle - signin - signup

--- ecommerceapp/lib/src/routing/app_route.dart
+++ ecommerceapp/lib/src/routing/app_route.dart
@@ -5,25 +5,23 @@ import 'package:go_router/go_router.dart';
 
 enum AppRoute {
   signIn,
-  home,
   profile,
 }
+
 final goRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
     initialLocation: '/sign-in',
     debugLogDiagnostics: true,
     routes: [
       GoRoute(
-        path: '/sign-in',
-        name: AppRoute.signIn.name,
-        builder: (context, state) 
-          => const CustomSignInScreen()
-          ),
+          path: '/sign-in',
+          name: AppRoute.signIn.name,
+          builder: (context, state) => const CustomSignInScreen()),
       GoRoute(
-         path: 'profile',
-            name: AppRoute.profile.name,
-            builder: (context, state) => const CustomProfileScreen(),
-          ),
+        path: 'profile',
+        name: AppRoute.profile.name,
+        builder: (context, state) => const CustomProfileScreen(),
+      ),
     ],
   );
 });
--- ecommerceapp/lib/src/screens/custom_sign_in_screen.dart
+++ ecommerceapp/lib/src/screens/custom_sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -5,10 +6,17 @@ class CustomSignInScreen extends ConsumerWidget {
   const CustomSignInScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref)  {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authProviders = <AuthProvider>[
+      EmailAuthProvider(),
+      PhoneAuthProvider(),
+    ];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sign In'),
+      ),
+      body: SignInScreen(
+        providers: authProviders,
       ),
     );
   }
