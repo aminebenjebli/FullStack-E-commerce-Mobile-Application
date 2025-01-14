3904f742e627ac736cd7125123376d8a4c261082
aminebenjebli <80284883+aminebenjebli@users.noreply.github.com>
2025-01-14 20:00:17 +0100

    firestore_data_serialization _read- update - delete

--- ecommerceapp/lib/src/data/firestore_repository.dart
+++ ecommerceapp/lib/src/data/firestore_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerceapp/src/data/job.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -15,13 +16,30 @@ class FirestoreRepository {
   FirestoreRepository(this._firestore);
   final FirebaseFirestore _firestore;
 
-  Future<void> addJob(String uid, String title, String company) async {
-    final docRef = await _firestore.collection('jobs').add({
-      'uid': uid,
-      'title': title,
-      'company': company,
-    });
-    debugPrint(docRef.id);
+  // A method to add a new job to Firestore. Create
+  Future<void> addJob(String uid, String title, String company) =>
+      _firestore.collection('jobs').add({
+        'uid': uid,
+        'title': title,
+        'company': company,
+      });
+  // A method to update a job in Firestore. Update
+  Future<void> updateJob(
+          String uid, String jobId, String title, String company) =>
+      _firestore.doc('jobs/$jobId').update({
+        'uid': uid,
+        'title': title,
+        'company': company,
+      });
+  // A method to delete a job from Firestore. delete
+  Future<void> deleteJob(String uid, String jobId) =>
+      _firestore.doc('jobs/$jobId').delete();
+
+  // A query to fetch all jobs from Firestore. Read
+  Query<Job> jobsQuery() {
+    return _firestore.collection('jobs').withConverter(
+        fromFirestore: (snapshot, _) => Job.fromMap(snapshot.data()!),
+        toFirestore: (job, _) => job.toMap());
   }
 }
 
--- ecommerceapp/lib/src/screens/home_screen.dart
+++ ecommerceapp/lib/src/screens/home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerceapp/src/data/firestore_repository.dart';
+import 'package:ecommerceapp/src/data/job.dart';
 import 'package:ecommerceapp/src/routing/app_route.dart';
-import 'package:faker/faker.dart';
+import 'package:faker/faker.dart' hide Job;
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -17,6 +20,7 @@ class HomeScreen extends ConsumerWidget {
           onPressed: () => context.goNamed(AppRoute.profile.name),
         )
       ]),
+      body: const JobsListView(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
@@ -29,6 +33,42 @@ class HomeScreen extends ConsumerWidget {
               .addJob(user!.uid, title, company);
         },
       ),
+    );
+  }
+}
+
+class JobsListView extends ConsumerWidget {
+  const JobsListView({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final firestoreRepository = ref.watch(firestoreRepositoryProvider);
+    return FirestoreListView<Job>(
+      query: firestoreRepository.jobsQuery(),
+      itemBuilder: (BuildContext context, QueryDocumentSnapshot<Job> doc) {
+        final job = doc.data();
+        return Dismissible(
+          key: Key(doc.id),
+          background: const ColoredBox(color: Colors.red),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+            final user = ref.read(firebaseAuthProvider).currentUser;
+            ref.read(firestoreRepositoryProvider).deleteJob(user!.uid, doc.id);
+          },
+          child: ListTile(
+            title: Text(job.title),
+            subtitle: Text(job.company),
+            onTap: () {
+              final user = ref.read(firebaseAuthProvider).currentUser;
+              final faker = Faker();
+              final title = faker.job.title();
+              final company = faker.company.name();
+              ref
+                  .read(firestoreRepositoryProvider)
+                  .updateJob(user!.uid, doc.id, title, company);
+            },
+          ),
+        );
+      },
     );
   }
 }
